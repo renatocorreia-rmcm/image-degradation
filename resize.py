d980a4b0466fdac89a9eee85c36fe36116f1cd9a
renatocorreia-rmcm <rmcm@cin.ufpe.br>
2026-03-25 21:24:55 -0300

    sketch for resizing w/ interpolation

--- resize.py
+++ resize.py
@@ -2,6 +2,7 @@ import math
 
 import numpy as np
 import cv2
+from numpy import interp
 
 
 def resize(old_img: np.ndarray, new_w: int, new_h: int):
@@ -11,10 +12,8 @@ def resize(old_img: np.ndarray, new_w: int, new_h: int):
 	old_w = old_img.shape[1]
 	old_h = old_img.shape[0]
 
-	if new_h == 0:
-		new_h = int(old_h*(new_w/old_w))
-
 	new_img = np.zeros(shape=([new_h, new_w, 3]), dtype='uint8')
+
 	for new_i, line in enumerate(new_img):
 		for new_j, pixl in enumerate(line):
 
@@ -27,8 +26,46 @@ def resize(old_img: np.ndarray, new_w: int, new_h: int):
 	return new_img
 
 
-img = cv2.imread('assets/cat.jpg')
-cv2.imshow('resized', resize(img, new_w=30, new_h=30))
+def grid_interpolate(vertices: np.ndarray, values: np.ndarray, target: np.ndarray) -> np.ndarray:
+	x0 = vertices[0][0]
+
+
+def resize_interp(old_img: np.ndarray, new_w: int, new_h: int):
+	"""
+		interpolate empty pixels
+	"""
+	old_w = old_img.shape[1]
+	old_h = old_img.shape[0]
+
+	new_img = np.zeros(shape=([new_h, new_w, 3]), dtype='uint8')
+	for new_i, line in enumerate(new_img):
+		for new_j, pixl in enumerate(line):
+
+			# discover equivalent coordinate in old image
+			old_j = new_j*(old_w/new_w)
+			old_i = new_i*(old_h/new_h)
+
+			# interpolate (old_i, old_j) inside
+			# (floor(old_i), floor(old_j))  (floor(old_i), ceil(old_j))
+			# (ceil(old_i), floor(old_j))  (ceil(old_i), ceil(old_j))
+
+			vertices = np.array([
+				(math.floor(old_i), math.floor(old_j)), (math.floor(old_i), math.ceil(old_j)),
+				(math.ceil(old_i), math.floor(old_j)), (math.ceil(old_i), math.ceil(old_j))
+			])
+			values = np.array([
+				old_img[vertices[0][0]][vertices[0][1]], old_img[vertices[1][0]][vertices[1][1]],
+				old_img[vertices[2][0]][vertices[2][1]], old_img[vertices[3][0]][vertices[3][1]],
+			])
+			target = np.array([old_i, old_j])
+
+			#new_img[new_i][new_j] = grid_interpolate(vertices=vertices, values=values, target=target)
+
+	return new_img
+
+
+img = cv2.imread('assets/gradiente.png')
+cv2.imshow('resized', resize_interp(img, new_w=7, new_h=7))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
